78e3c05fd8fa915e7901114889665d5420720b60
nbravofrank <nickbravofrank@gmail.com>
2021-03-25 11:36:33 -0700

    Low Power Lib Test

--- AnnoyMyke/src/main.cpp
+++ AnnoyMyke/src/main.cpp
@@ -1,11 +1,24 @@
-#include <Arduino.h>
+#include "Arduino.h"
+#include "ArduinoLowPower.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  // Uncomment this function if you wish to attach function dummy when RTC wakes up the chip
+  // LowPower.attachInterruptWakeup(RTC_ALARM_WAKEUP, dummy, CHANGE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello World");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  // Triggers a 2000 ms sleep (the device will be woken up only by the registered wakeup sources and by internal RTC)
+  // The power consumption of the chip will drop consistently
+  LowPower.sleep(2000);
+}
+
+void dummy() {
+  // This function will be called once on device wakeup
+  // You can do some little operations here (like changing variables which will be used in the loop)
+  // Remember to avoid calling delay() and long running functions since this functions executes in interrupt context
 }
